6a9b1552b0653a5026d7dcc848db6e086073fee4
vitaliigulimov <vitaliygulimov@ukr.net>
2025-06-13 09:13:54 +0300

    fix mainjsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './components/App/App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        {' '}
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 );
